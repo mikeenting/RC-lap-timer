233c80dbed7f680f9e8c1b4e23ee7975dc319133
Mike Enting <mikeenting@hotmail.com>
2023-07-01 20:54:29 +0200

    Added demo

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # RC-lap-timer
 ## Description
-This projects uses an Arduino to track the laptimes of an RC car. It times the lap, and keeps track of the fastest registered lap. It is portable and can be powered with a 9V battery. See [this link](https://www.thingiverse.com/thing:6049245) to find the project on Thingiverse
+This projects uses an Arduino to track the lap times of an RC car. It times the lap, and keeps track of the fastest registered lap. It is portable and can be powered with a 9V battery. See [this link](https://www.thingiverse.com/thing:6049245) to find the project on Thingiverse.
 
 ## Demo
-*To be added*
+[![Demo](https://img.youtube.com/vi/-q12BmalPtk/0.jpg)](https://youtu.be/-q12BmalPtk)
+
 
 ## Required materials
 See component list [here](https://github.com/mikeenting/RC-lap-timer/blob/main/laptimer_component_list.csv)
@@ -11,15 +12,15 @@ See component list [here](https://github.com/mikeenting/RC-lap-timer/blob/main/l
 ## Assembly
 1. Download [this program](https://github.com/mikeenting/RC-lap-timer/blob/main/laptimer/laptimer.ino) and upload it to your Arduino
 2. Download, slice and print all files in [this folder](https://github.com/mikeenting/RC-lap-timer/blob/main/3d_designs) (see suggested printer settings below)
-2. Use M3 heat set insert to connect arduino to the floor
-2. Connect ultrasonic sensor to middle bottom
-3. Connect floor to middle bottom using M3 heat set inserts
-4. Use hot glue to connect potentiometer to middle bottom
-5. Connect LCD to middle top
-6. Wire up Arduino with all parts, according to [this circuit view](https://github.com/mikeenting/RC-lap-timer/blob/main/laptimer_circuit_view.png)
-7. Connect middle bottom and top using M3 heat set inserts
-8. Attach the lid using M3 heat set inserts
-9. Power up the Arduino and you're good to go!
+3. Use M3 heat set insert to connect arduino to the floor
+4. Connect ultrasonic sensor to middle bottom
+5. Connect floor to middle bottom using M3 heat set inserts
+6. Use hot glue to connect potentiometer to middle bottom
+7. Connect LCD to middle top
+8. Wire up Arduino with all parts, according to [this circuit view](https://github.com/mikeenting/RC-lap-timer/blob/main/laptimer_circuit_view.png)
+9. Connect middle bottom and top using M3 heat set inserts
+10. Attach the lid using M3 heat set inserts
+11. Power up the Arduino and you're good to go!
 
 ## Printer Settings
 I printed with a Creality Ender 3 V2, using 1.75mm PLA and the following settings:
--- laptimer/laptimer.ino
+++ laptimer/laptimer.ino
@@ -1,19 +1,19 @@
 // This includes the LiquidCrystal library, which is used to control the LCD display.
 #include <LiquidCrystal.h>
 
-#define echoPin 8 // attach pin D2 Arduino to pin Echo of HC-SR04
-#define trigPin 9 // attach pin D3 Arduino to pin Trig of HC-SR04
+#define echoPin 8  // attach pin D2 Arduino to pin Echo of HC-SR04
+#define trigPin 9  // attach pin D3 Arduino to pin Trig of HC-SR04
 
 // Adaptive variables
-int maxdistance = 15; // variable for setting the default distance in centimeters measured by the sensor
-int minlaptime = 5;   // variable for setting the minimum number of seconds a lap should take (to prevent invalid laps from being registered)
-int maxlaps = 10;     // variable for setting the maximum number of laps the timer should register. Do not set higher than 99, to prevent the lap numbers from rolling off the LCD screen.
+int maxdistance = 15;  // variable for setting the default distance in centimeters measured by the sensor
+int minlaptime = 5;    // variable for setting the minimum number of seconds a lap should take (to prevent invalid laps from being registered)
+int maxlaps = 3;      // variable for setting the maximum number of laps the timer should register. Do not set higher than 99, to prevent the lap numbers from rolling off the LCD screen.
 
 // Defines variables
-long duration;     // variable for the duration of sound wave travel
-int distance;      // variable for the distance measurement
-int lap = 0;       // variable for counting the amount of laps
-int recordlap = 1; // variable for counting which lap the fastest lap was set
+long duration;      // variable for the duration of sound wave travel
+int distance;       // variable for the distance measurement
+int lap = 0;        // variable for counting the amount of laps
+int recordlap = 1;  // variable for counting which lap the fastest lap was set
 double laptime = 0;
 double recordtime = 0;
 double livelaptime = 0;
@@ -33,41 +33,42 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 // Finish Flag
 byte flag1[8] = {
-    B11111,
-    B00000,
-    B11111,
-    B00000,
-    B11111,
-    B00000,
-    B11111,
-    B00000};
+  B11111,
+  B00000,
+  B11111,
+  B00000,
+  B11111,
+  B00000,
+  B11111,
+  B00000
+};
 
 byte flag2[8] = {
-    B00000,
-    B11111,
-    B00000,
-    B11111,
-    B00000,
-    B11111,
-    B00000,
-    B11111};
+  B00000,
+  B11111,
+  B00000,
+  B11111,
+  B00000,
+  B11111,
+  B00000,
+  B11111
+};
 
-void setup()
-{
+void setup() {
   // Setup ultrasonic sensor HC-SR04
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
-  pinMode(echoPin, INPUT);  // Sets the echoPin as an INPUT
+  pinMode(trigPin, OUTPUT);  // Sets the trigPin as an OUTPUT
+  pinMode(echoPin, INPUT);   // Sets the echoPin as an INPUT
 
   // Setup serial monitor
-  Serial.begin(9600);                            // // Serial Communication is starting with 9600 of baudrate speed
-  Serial.println("Welcome to Arduino laptimer"); // print some text in Serial Monitor
+  Serial.begin(9600);                             // // Serial Communication is starting with 9600 of baudrate speed
+  Serial.println("Welcome to Arduino laptimer");  // print some text in Serial Monitor
 
   // Setup LCD
-  lcd.begin(16, 2);              // set up number of columns and rows
-  lcd.setCursor(0, 0);           // move cursor to   (0, 0)
-  lcd.print("Welcome to");       // print message at (0, 0)
-  lcd.setCursor(0, 1);           // move cursor to   (0, 0)
-  lcd.print("Arduino laptimer"); // print message at (0, 0)
+  lcd.begin(16, 2);               // set up number of columns and rows
+  lcd.setCursor(0, 0);            // move cursor to   (0, 0)
+  lcd.print("Welcome to");        // print message at (0, 0)
+  lcd.setCursor(0, 1);            // move cursor to   (0, 0)
+  lcd.print("Arduino laptimer");  // print message at (0, 0)
   delay(4000);
   lcd.clear();
   lcd.setCursor(0, 0);
@@ -75,11 +76,10 @@ void setup()
   lcd.setCursor(0, 1);
   lcd.print("get started!");
 
-  lcd.createChar(0, flag1); // Creates custom character and stores it in the LCD's memory
-  lcd.createChar(1, flag2); // Creates second custom character
+  lcd.createChar(0, flag1);  // Creates custom character and stores it in the LCD's memory
+  lcd.createChar(1, flag2);  // Creates second custom character
 }
-void loop()
-{
+void loop() {
   delay(10);
   // Clears the trigPin condition
   digitalWrite(trigPin, LOW);
@@ -91,12 +91,11 @@ void loop()
   // Reads the echoPin, returns the sound wave travel time in microseconds
   duration = pulseIn(echoPin, HIGH);
   // Calculating the distance
-  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+  distance = duration * 0.034 / 2;  // Speed of sound wave divided by 2 (go and back)
 
   // This loop is used to wait for the first lap to start.
   // It continuously measures the distance until the distance is less than maxdistance, indicating that the first lap has started.
-  while (lap < 1)
-  {
+  while (lap < 1) {
     lcd.setCursor(0, 0);
     lcd.print("Start racing to");
     lcd.setCursor(0, 1);
@@ -112,17 +111,28 @@ void loop()
     // Reads the echoPin, returns the sound wave travel time in microseconds
     duration = pulseIn(echoPin, HIGH);
     // Calculating the distance
-    distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
-    if (distance < maxdistance)
-    {
+    distance = duration * 0.034 / 2;  // Speed of sound wave divided by 2 (go and back)
+    if (distance < maxdistance) {
       lap++;
       a = millis();
       e = millis();
     }
   }
 
-  while (lap <= maxlaps)
-  {
+  while (lap <= maxlaps) {
+    // Clears the trigPin condition
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    // Reads the echoPin, returns the sound wave travel time in microseconds
+    duration = pulseIn(echoPin, HIGH);
+    // Calculating the distance
+    distance = duration * 0.034 / 2;  // Speed of sound wave divided by 2 (go and back)
+
+
     // Display current race time to display
     d = millis();
     c = millis();
@@ -143,18 +153,14 @@ void loop()
     // This conditional block is executed when the distance is less than maxdistance, indicating that a lap has ended.
     // It then checks whether the lap time is more than the minimum lap time.
     // If it is, it updates the record time if necessary, increments the lap count, and displays the lap time and record time on the LCD.
-    if (distance < maxdistance)
-    {
+    if (distance < maxdistance) {
       c = millis();
       laptime = (c - a) / 1000;
-      if (laptime > minlaptime)
-      {
-        if (recordtime == 0)
-        {
+      if (laptime > minlaptime) {
+        if (recordtime == 0) {
           recordtime = laptime;
         }
-        if (laptime < recordtime)
-        {
+        if (laptime < recordtime) {
           recordtime = laptime;
           recordlap = lap;
         }
@@ -173,7 +179,7 @@ void loop()
         a = millis();
         // Write information to LCD Display
         lcd.clear();
-        lcd.setCursor(0, 0); // move cursor to   (0, 0)
+        lcd.setCursor(0, 0);  // move cursor to   (0, 0)
         lcd.print("Time:");
         lcd.setCursor(6, 0);
         lcd.print(laptime);
@@ -192,8 +198,7 @@ void loop()
         delay(3000);
         lcd.clear();
         lap++;
-        if (lap == 11)
-        {
+        if (lap == 11) {
           Serial.print("Average time per lap over past 10 laps:");
           Serial.print("\t\t");
           Serial.println(totaltime_seconds / 10);
@@ -201,27 +206,21 @@ void loop()
       }
     }
   }
-  while (lap > maxlaps)
-  {
+  while (lap > maxlaps) {
     // Display flag pattern on the first half of the LCD
-    for (int i = 0; i < 2; i++)
-    { // For each row
-      for (int j = 0; j < 8; j++)
-      { // For each column in the first half
+    for (int i = 0; i < 2; i++) {    // For each row
+      for (int j = 0; j < 8; j++) {  // For each column in the first half
         lcd.setCursor(j, i);
-        if (j % 2 == i % 2)
-        {
+        if (j % 2 == i % 2) {
           lcd.write((uint8_t)0);
-        }
-        else
-        {
+        } else {
           lcd.write((uint8_t)1);
         }
       }
     }
 
     // Display 'Finish' and fastest time on the second half of the LCD
-    lcd.setCursor(9, 0); // Set cursor at 9th column of the first row
+    lcd.setCursor(9, 0);  // Set cursor at 9th column of the first row
     lcd.print("Finish!");
     delay(3000);
     lcd.clear();
